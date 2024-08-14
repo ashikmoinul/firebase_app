229bd4419e079b59f51e2793a0c250b0710d3fd7
ashik <ashik.moinul150@gmail.com>
2024-08-15 01:04:06 +0600

    Stream builder, Snapshots, Firebase Firestore

--- lib/ui/screens/match_list_screen.dart
+++ lib/ui/screens/match_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 import '../../entities/football.dart';
 import '../widgets/football_score_card.dart';
 
@@ -29,9 +30,7 @@ class _MatchListScreenState extends State<MatchListScreen> {
         ),
       );
     }
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   @override
@@ -47,14 +46,50 @@ class _MatchListScreenState extends State<MatchListScreen> {
         title: const Text('Football'),
         backgroundColor: Colors.blue,
       ),
-      body: ListView.builder(
-          itemCount: matchList.length,
-          itemBuilder: (context, index) {
-            return FootballScoreCard(
-
-              football: matchList[index],
+      body: StreamBuilder(
+        stream: firebaseFirestore.collection('football').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          }),
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          if (snapshot.hasData == false) {
+            return const Center(
+              child: Text('Empty list'),
+            );
+          }
+          matchList.clear();
+          for (QueryDocumentSnapshot doc in snapshot.data?.docs ?? []) {
+            matchList.add(
+              Football(
+                matchName: doc.id,
+                team1Name: doc.get('team1Name'),
+                team2Name: doc.get('team2Name'),
+                team1Score: doc.get('team1'),
+                team2Score: doc.get('team2'),
+              ),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: matchList.length,
+            itemBuilder: (context, index) {
+              return FootballScoreCard(
+                football: matchList[index],
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
